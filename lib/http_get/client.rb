badb37fc726589c20a4d2b51cd56f3fb32796790
Erik Strömberg <erik.stromberg@gmail.com>
2015-11-09 15:14:38 +0100

    .

--- lib/http_get/client.rb
+++ lib/http_get/client.rb
@@ -1,5 +1,5 @@
 require 'httpclient'
-require 'addressable'
+require 'addressable/uri'
 
 class HttpGet::Client
   ::USER_AGENTS ||= ["Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"]
